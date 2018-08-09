60f7684317b4eda54f36ba59d92d89c586633324
AndrewSC208 <meilingandrew@gmail.com>
2018-08-09 07:35:04 -0700

    finished albums app, with a new button component, and some changes to the card details component, then finally some small stylistic changes to match what I like

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,10 @@ import React from "react";
 import { Text, View } from "react-native";
 
 const Header = ({ headerText }) => {
-    const { textStyle, viewStyle } = styles;
+    const { 
+        textStyle, 
+        viewStyle 
+    } = styles;
 
     return (
         <View style={viewStyle}>
